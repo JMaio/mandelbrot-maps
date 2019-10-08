50a01df8ac27745ef6ef3bb9e66f1054a4d2079d
JMaio <10330349+JMaio@users.noreply.github.com>
2019-10-08 11:43:52 +0100

    display mandelbrot on react render

--- src/components/MandelbrotRenderer.jsx
+++ src/components/MandelbrotRenderer.jsx
@@ -7,35 +7,22 @@ import { Typography, Button } from "@material-ui/core";
 
 export default function MandelbrotRenderer(props) {
 
-  const canvas = useRef("canvas");
+  const canvas = useRef(null);
   const canvasSize = 600;
 
   const bounds = {x: [-1.5, 0.5], y: [-1, 1]};
   const maxI = 63;
 
-  const [ctx, setCtx] = useState(null)
+  // const [ctx, setCtx] = useState(null)
 
   const [toggle, setToggle] = useState(false);
-
-  useEffect(() => {
-    setCtx(canvas.current.getContext("2d"));
-  }, []);
-
-  // function linspace(start, stop, cardinality) {
-  //   var arr = [];
-  //   var step = (stop - start) / (cardinality - 1);
-  //   for (var i = 0; i < cardinality; i++) {
-  //     arr.push(start + (step * i));
-  //   }
-  //   return arr;
-  // }
 
   function linspace(start, stop, cardinality) {
     let step = (stop - start) / cardinality;
     return _.range(start, stop, step);
   }
 
-  const fillDiag = () => {
+  const fillDiag = ctx => {
     ctx.fillStyle = "#444";
 
     for (let i = 0; i < canvasSize; i++) {
@@ -44,7 +31,7 @@ export default function MandelbrotRenderer(props) {
     }
   }
 
-  const fillProcFast = () => {
+  const fillProcFast = ctx => {
     const [xl, xr] = bounds.x;
     const [yb, yt] = bounds.y;
 
@@ -79,24 +66,10 @@ export default function MandelbrotRenderer(props) {
         }
       });
     });
-    
-    // linspace(xl, xr, canvasSize).forEach((re, x) => {
-    //   let c = Complex(re, 0);
-    //     var z = Complex.ZERO;
-    //     var draw = true;
-    //     // console.log(z.toString());
-    //     for (let iter = 0; iter < maxI; iter++) {
-    //       z = z.mul(z).add(c);
-    //       if (z.abs() > 4) { draw = false; break; }
-    //     }
-    //     if (draw) {
-    //       ctx.fillRect(x, y, 1, 1);
-    //       ctx.fillRect(x, (canvasSize - y)-1, 1, 1);
-    //     }
-    // }
   };
 
-  const fillProc = () => {
+
+  const fillProc = ctx => {
     const [xl, xr] = bounds.x;
     const [yb, yt] = bounds.y;
     linspace(xl, xr, canvasSize).forEach((re, x) => {
@@ -113,18 +86,9 @@ export default function MandelbrotRenderer(props) {
         ctx.fillRect(x, y, 1, 1);
       });
     });
-    // for (let re = 0; re < canvasSize; re++) {
-    //   // console.log(new Complex(re, 1));
-    //   for (let im = 0; im < canvasSize; im++) {
-    //     for (let iter = 0; iter < maxI; iter++) {
-    //       // 1
-    //     }
-    //     ctx.fillRect(re, im, re+1, im+1);
-    //   }
-    // }
   };
 
-  const toggleRect = () => {
+  const toggleRect = ctx => {
     const fill = () => ctx.fillRect(0, 0, 200, 200);
     const clear = () => ctx.clearRect(0, 0, 200, 200);
     toggle ? clear() : fill();
@@ -136,6 +100,13 @@ export default function MandelbrotRenderer(props) {
 
   const [ptime, setPtime] = useState(0);
   const [ptime2, setPtime2] = useState(0);
+
+  useEffect(() => {
+    console.log("canvas loaded");
+    const ctx = canvas.current.getContext("2d");
+    // canvas.current.ready(() => {
+    fillProcFast(ctx);
+  }, [canvas, fillProcFast])
 
   return (
     <div 
@@ -154,18 +125,11 @@ export default function MandelbrotRenderer(props) {
         width={canvasSize}
         height={canvasSize}
         ref={canvas} 
-
-
-        // onClick={e => {
-        //   let t0 = performance.now();
-        //   fillProc();
-        //   let t1 = performance.now()
-        //   setPtime(t1 - t0);
-        // }}
-        // width="100%"
-        // height="100%" 
-      />
-      <Button color="primary" onClick={e => {
+        // onLoad={fillProcFast()}
+      />{
+        // , [fillProcFast])
+      }
+      {/* <Button color="primary" onClick={e => {
           let t0 = performance.now();
           fillProc();
           let t1 = performance.now()
@@ -177,7 +141,7 @@ export default function MandelbrotRenderer(props) {
           fillProcFast();
           let t1 = performance.now()
           setPtime2(t1 - t0);
-        }}>{ptime2}</Button>
+        }}>{ptime2}</Button> */}
     </div>
   )
 }
