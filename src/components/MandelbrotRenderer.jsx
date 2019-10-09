76be440d0f37e8689644dfc6d451a99e453f8fa3
JMaio <10330349+JMaio@users.noreply.github.com>
2019-10-09 14:18:01 +0100

    add buttons for testing

--- src/components/MandelbrotRenderer.jsx
+++ src/components/MandelbrotRenderer.jsx
@@ -1,7 +1,8 @@
 import React, { useRef, useEffect, useState } from "react";
 import Complex from 'complex.js';
 import _ from 'lodash';
-import { Typography, Button } from "@material-ui/core";
+import { Typography, Button, Input, Slider } from "@material-ui/core";
+import { resolve } from "q";
 // import { performance } from 'perf_hooks';
 
 
@@ -11,9 +12,9 @@ export default function MandelbrotRenderer(props) {
   const canvasSize = 600;
 
   const bounds = {x: [-1.5, 0.5], y: [-1, 1]};
-  const maxI = 63;
+  const [maxI, setMaxI] = useState(20);
 
-  // const [ctx, setCtx] = useState(null)
+  const [ctx, setCtx] = useState(null);
 
   const [toggle, setToggle] = useState(false);
 
@@ -80,12 +81,37 @@ export default function MandelbrotRenderer(props) {
         // console.log(z.toString());
         for (let iter = 0; iter < maxI; iter++) {
           z = z.mul(z).add(c);
-          if (z.abs() > 4) { draw = false; break; }
+          if (z.abs() > 2.0) { draw = false; break; }
         }
         ctx.fillStyle = draw ? "#000" : "#fff";
         ctx.fillRect(x, y, 1, 1);
       });
     });
+  };
+
+  const fillProcAsync = ctx => {
+    console.log("entered fillproc");
+    return new Promise(resolve => {
+      const [xl, xr] = bounds.x;
+      const [yb, yt] = bounds.y;
+      ctx.fillStyle = "#ddd";
+      linspace(xl, xr, canvasSize).forEach((re, x) => {
+        linspace(yb, yt, canvasSize).forEach((im, y) => {
+          let c = Complex(re, im);
+          var z = Complex.ZERO;
+          var draw = true;
+          // console.log(z.toString());
+          for (let iter = 0; iter < maxI; iter++) {
+            z = z.mul(z).add(c);
+            if (z.abs() > 2.0) { draw = false; break; }
+          }
+          if (draw) {
+            ctx.fillRect(x, y, 1, 1);
+          }
+        });
+      });
+      resolve();
+    })
   };
 
   const toggleRect = ctx => {
@@ -100,12 +126,14 @@ export default function MandelbrotRenderer(props) {
 
   const [ptime, setPtime] = useState(0);
   const [ptime2, setPtime2] = useState(0);
+  const [ptime3, setPtime3] = useState(0);
 
   useEffect(() => {
-    console.log("canvas loaded");
+    // console.log("canvas loaded");
     const ctx = canvas.current.getContext("2d");
+    setCtx(ctx);
     // canvas.current.ready(() => {
-    fillProcFast(ctx);
+    // fillProcFast(ctx);
   })
 
 
@@ -127,19 +155,48 @@ export default function MandelbrotRenderer(props) {
         height={canvasSize}
         ref={canvas} 
       />
-      {/* <Button color="primary" onClick={e => {
+      <Button variant="contained" color="primary" onClick={e => {
           let t0 = performance.now();
-          fillProc();
+          fillProc(ctx);
           let t1 = performance.now()
           setPtime(t1 - t0);
-        }}>{ptime}</Button>
+        }}>{ptime.toFixed(2)}</Button>
 
-      <Button color="primary" onClick={e => {
+      <Button variant="contained" color="primary" onClick={e => {
           let t0 = performance.now();
-          fillProcFast();
+          fillProcFast(ctx);
           let t1 = performance.now()
           setPtime2(t1 - t0);
-        }}>{ptime2}</Button> */}
+        }
+      }>{ptime2.toFixed(2)}</Button>
+      
+      <Button variant="contained" color="secondary" onClick={e => async function () {
+          let t0 = performance.now();
+          await fillProcAsync(ctx);
+          let t1 = performance.now()
+          setPtime3(t1 - t0);
+        }()
+      }>{ptime3.toFixed(2)}</Button>
+
+      <Button variant="outlined" onClick={e => {
+        let c = canvas.current;
+        ctx.clearRect(0, 0, c.width, c.height)
+      }}>clear</Button>
+
+      <hr />
+
+      <Slider
+        defaultValue={maxI}
+        valueLabelDisplay="on"
+        step={1}
+        min={1}
+        max={100}
+        style={{ 
+          width: 200,
+          margin: 30,
+        }}
+        onChange={ (e, v) => setMaxI(v) }
+      />
     </div>
   )
 }
