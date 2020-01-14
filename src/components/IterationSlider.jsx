704f919d07e6a3391501b47a4fefe2b27706f888
Joao Maio <10330349+JMaio@users.noreply.github.com>
2020-01-14 20:46:26 +0000

    Create IterationSlider.jsx

---
+++ src/components/IterationSlider.jsx
@@ -0,0 +1,20 @@
+import React from "react";
+import { Slider } from "@material-ui/core";
+
+export default function IterationSlider(props) {
+
+  return (
+    <Slider
+      defaultValue={props.maxIter}
+      valueLabelDisplay="on"
+      step={1}
+      min={1}
+      max={100}
+      style={{
+        width: 200,
+        margin: 30,
+      }}
+      onChange={(e, v) => props.setMaxIter(v)}
+    />
+  )
+}
