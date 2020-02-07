b42ef869400d7572ac192658adb9b6cd956e853b
Joao Maio <10330349+JMaio@users.noreply.github.com>
2020-02-07 20:08:32 +0000

    update app to handle resizes and fix layout

--- src/App.js
+++ src/App.js
@@ -1,18 +1,20 @@
 import React, { Fragment, useState } from 'react';
 import './App.css';
 import { Grid } from '@material-ui/core';
-import ZoomBar, { } from './components/ZoomBar';
+import ZoomBar from './components/ZoomBar';
 import IterationSlider from './components/IterationSlider';
 import RotationControl from './components/RotationControl';
-// import * from "./renderers/";
 
 import 'typeface-roboto';
 import MandelbrotRenderer from './components/MandelbrotRenderer.jsx';
 import { useSpring } from 'react-spring';
 import JuliaRenderer from './components/JuliaRenderer';
+import { useWindowSize } from './components/utils';
 
 
 function App() {
+
+  const size = useWindowSize();
 
   let defaultSpringConfig = { mass: 1, tension: 100, friction: 200 };
 
@@ -43,54 +45,30 @@ function App() {
       config: { mass: 1, tension: 600, friction: 50 },
     })),
   }
-  // const controlPos = useSpring(() => ({
-  //   pos: [0, 0],
-  //   config: defaultSpringConfig,
-  // }));
-
-  // const controlRot = useSpring(() => ({
-  //   theta: 0,
-  //   last_pointer_angle: 0,
-  //   itheta: 0,
-
-  //   config: defaultSpringConfig,
-  // }))
-
-  // const controlZoom = useSpring(() => ({
-  //   zoom: 1.0,
-  //   last_pointer_dist: 0,
-
-  //   minZoom: 0.5,
-  //   maxZoom: 100000,
-
-  //   config: { mass: 1, tension: 600, friction: 50 },
-  // }))
-
+  
   return (
     <Fragment>
       <Grid
         container
-        direction="column"
-        justify="space-evenly"
-        alignItems="flex-end"
+        direction={size.width < size.height ? "column" : "row"}
+        justify="center"
+        className="fullSize"
+        style={{
+          position: "absolute",
+        }}
       >
-        <MandelbrotRenderer
-          // style={{
-          //   position: 'absolute',
-          //   zIndex: 0,
-          // }}
-          controls={mandelbrotControls}
-          // pos={controlPos}
-          // // screenpos={controlScreenPos}
-          // rot={controlRot}
-          // zoom={controlZoom}
-          maxiter={maxI}
-          // renderer={glRenderer ? }
-        />
-        <JuliaRenderer
-          controls={mandelbrotControls}
-          maxiter={maxI}
-        />
+        <Grid item xs className="renderer">
+          <MandelbrotRenderer
+            controls={mandelbrotControls}
+            maxiter={maxI}
+          />
+        </Grid>
+        <Grid item xs className="renderer">
+          <JuliaRenderer
+            controls={mandelbrotControls}
+            maxiter={maxI}
+          />
+        </Grid>
       </Grid>
       <Grid
         container
