c61598d14ee23b48cf4497fd184cc6dda0167c4f
Joao Maio <10330349+JMaio@users.noreply.github.com>
2020-02-23 20:20:25 +0000

    move touchBinds to generic touch bind

--- src/components/MandelbrotRenderer.jsx
+++ src/components/MandelbrotRenderer.jsx
@@ -1,15 +1,12 @@
 import React, { useRef, useEffect } from "react";
 import _ from 'lodash';
 
-import { addV, useGesture } from "react-use-gesture";
-import { scale } from 'vec-la';
-
+import { useGesture } from "react-use-gesture";
 import { animated } from "react-spring";
-
 
 import newSmoothMandelbrotShader from "../shaders/newSmoothMandelbrotShader";
 import WebGLCanvas from "./WebGLCanvas";
-// import mShader from "../shaders/smooth_mandelbrot_shader.glsl";
+import { genericTouchBind } from "./utils";
 
 export default function MandelbrotRenderer(props) {
 
@@ -32,9 +29,9 @@ export default function MandelbrotRenderer(props) {
 
 
   // read incoming props
-  const [{ pos }, setControlPos] = props.controls.pos;
-  const [{ theta, last_pointer_angle }, setControlRot] = props.controls.rot;
-  const [{ zoom, last_pointer_dist, minZoom, maxZoom }, setControlZoom] = props.controls.zoom;
+  const [{ pos }] = props.controls.pos;
+  // const [{ theta, last_pointer_angle }, setControlRot] = props.controls.rot;
+  const [{ zoom }, setControlZoom] = props.controls.zoom;
   const maxI = props.maxiter;
   const AA = 1;
 
@@ -50,101 +47,19 @@ export default function MandelbrotRenderer(props) {
     radius: 30,
   });
 
-  // the hook responsible for handling gestures
-  const touchBind = useGesture({
-
-    // prevent some browser events such as swipe-based navigation or
-    // pinch-based zoom and instead redirect them to this handler
-    onDragStart:  ({ event }) => event.preventDefault(),
-    onPinchStart: ({ event }) => event.preventDefault(),
-
-    onPinch: ({ offset: [d], down, vdva, memo = [theta.getValue(), last_pointer_angle.getValue(), zoom.getValue(), last_pointer_dist.getValue()] }) => {
-      // alert(mx, my)
-      // let [theta, lpa] = memo
-      let [,, z, lpd] = memo;
-      // console.log(d);
-      let d_rel = d / 250;
-      let curr_zoom = zoom.getValue();
-
-      setControlZoom({
-        zoom: _.clamp(z + (d_rel - lpd) * Math.sign(curr_zoom) * Math.abs(curr_zoom ** 0.9), minZoom.getValue(), maxZoom.getValue()),
-        last_pointer_dist: d_rel,
-
-        immediate: down,
-        // config: { velocity: vd, decay: true }
-      });
-
-      // setControlRot({
-      //   theta: t + (a - lpa),
-      //   last_pointer_angle: a,
-
-      //   immediate: down,
-      //   // config: { velocity: va, decay: true }
-      // });
-
-      return memo;
-    },
-
-    onPinchEnd: ({ vdva: [, va] }) => {
-      setControlRot({
-        // set theta so it's remembered next time
-        theta: va,
-
-        // config: { velocity: va, decay: true }
-      });
-    },
-
-    onWheel: ({ movement: [, my], vxvy: [, vy] }) => {
-      // x, y obtained from event
-      let z = zoom.getValue();
-      let newZ = z * (1 - my * (my < 0 ? 2e-3 : 9e-4));
-
-      setControlZoom({
-        zoom: _.clamp(newZ, minZoom.getValue(), maxZoom.getValue()),
-        config: {
-          // bias velocity towards zooming in (vy negative )
-          // if zooming
-          velocity: _.clamp(vy * (vy < 0 ? 2.5 : 1.5), -10, 10), // * z**0.9 - my/15,
-        }
-      });
-    },
-
-    onDrag: ({ down, movement, velocity, direction, memo = pos.getValue() }) => {
-
-      // change according to this formula:
-      // move (x, y) in the opposite direction of drag (pan with cursor)
-      // divide by canvas size to scale appropriately
-      // multiply by 2 to correct scaling on viewport
-      // use screen multiplier for more granularity
-      let realZoom = gl.current.canvas.height * zoom.getValue() * screenScaleMultiplier;
-      
-      let plotMovement = scale(movement, -2/realZoom);
-
-      let relMove = [plotMovement[0], -plotMovement[1]];
-      let relDir  = [direction[0], -direction[1]];
-
-      setControlPos({
-        pos: addV(memo, relMove),                    // add the displacement to the starting position
-        immediate: down,                                  // immediately apply if the gesture is active
-        config: { 
-          velocity: scale(relDir, -velocity/realZoom),  // set the velocity (gesture momentum)
-          decay: true,
-        },
-      });
-
-      return memo;
-    },
-
-  }, { 
-    event: { passive: false, capture: false }, 
+  
+  let gtb = genericTouchBind({
     domTarget: canvasRef,
-    // The config object passed to useGesture has drag, wheel, scroll, pinch and move keys
-    // for specific gesture options. See here for more details.
-    // drag: {
-    //   bounds,
-    //   rubberband: true,
-    // }
+    posControl: props.controls.pos,
+    zoomControl: props.controls.zoom,
+    screenScaleMultiplier: screenScaleMultiplier,
+    gl: gl,
   });
+
+  let touchBind = useGesture(
+    gtb.binds,
+    gtb.config
+  );
 
   useEffect(touchBind, [touchBind]);  
 
--- src/components/utils.js
+++ src/components/utils.js
@@ -1,4 +1,7 @@
 import { useEffect, useState, useCallback } from "react";
+import { useGesture, subV, addV } from "react-use-gesture";
+import { scale } from "vec-la";
+import _ from "lodash";
 
 // https://usehooks.com/useWindowSize/
 export function useWindowSize() {
@@ -26,4 +29,103 @@ export function useWindowSize() {
   }, [getSize, isClient]); // Empty array ensures that effect is only run on mount and unmount
 
   return windowSize;
+}
+
+// a touchbind for re-using across renderers
+export function genericTouchBind({ domTarget, posControl, zoomControl, screenScaleMultiplier, gl }) {
+  let [{ pos }, setControlPos] = posControl;
+  let [{ zoom, minZoom, maxZoom }, setControlZoom] = zoomControl;
+  return {
+    binds: {
+
+    // prevent some browser events such as swipe-based navigation or
+    // pinch-based zoom and instead redirect them to this handler
+    onDragStart:  ({ event }) => event.preventDefault(),
+    onPinchStart: ({ event }) => event.preventDefault(),
+
+    onPinch: ({ vdva: [vd,], down, delta: [dx,], origin, first, memo = [pos.getValue()], z = zoom.getValue() }) => {
+      if (first) {
+        let [p] = memo;
+        return [p, origin];
+      }
+      // initial origin access
+      let [p, initialOrigin] = memo;
+      let newZ = z * (1 + dx * 5e-3);
+      let newZclamp = _.clamp(newZ, minZoom.getValue(), maxZoom.getValue());
+
+      let realZoom = gl.current.canvas.height * newZclamp * screenScaleMultiplier;
+      let plotMovement = scale(subV(origin, initialOrigin), -2/realZoom);
+      let relMove = [plotMovement[0], -plotMovement[1]];
+
+      setControlZoom({
+        zoom: newZclamp,
+        immediate: down,
+        config: {
+          // value needs revising, currently too slow
+          velocity: 10 * vd,
+        }
+      });
+
+      setControlPos({
+        pos: addV(p, relMove),                    // add the displacement to the starting position
+        immediate: down,                                  // immediately apply if the gesture is active
+      });
+
+      return memo;
+    },
+
+    onWheel: ({ movement: [, my], vxvy: [, vy], active, z = zoom.getValue() }) => {
+      // x, y obtained from event
+      let newZ = z * (1 - my * (my < 0 ? 3e-4 : 2e-4));
+
+      setControlZoom({
+        zoom: _.clamp(newZ, minZoom.getValue(), maxZoom.getValue()),
+        immediate: active,
+        config: {
+          // velocity: active ? 0 : 10,
+        }
+      });
+
+      return z;
+    },
+
+    onDrag: ({ down, movement, velocity, direction, pinching, memo = pos.getValue() }) => {
+
+      // let pinch handle movement
+      if (pinching) return;
+      // change according to this formula:
+      // move (x, y) in the opposite direction of drag (pan with cursor)
+      // divide by canvas size to scale appropriately
+      // multiply by 2 to correct scaling on viewport
+      // use screen multiplier for more granularity
+      let realZoom = gl.current.canvas.height * zoom.getValue() * screenScaleMultiplier;
+      
+      let plotMovement = scale(movement, -2/realZoom);
+
+      let relMove = [plotMovement[0], -plotMovement[1]];
+      let relDir  = [direction[0], -direction[1]];
+
+      setControlPos({
+        pos: addV(memo, relMove),                    // add the displacement to the starting position
+        immediate: down,                                  // immediately apply if the gesture is active
+        config: { 
+          velocity: scale(relDir, -velocity/realZoom),  // set the velocity (gesture momentum)
+          decay: true,
+        },
+      });
+
+      return memo;
+    },
+
+  },
+  config: { 
+    event: { passive: false, capture: false }, 
+    domTarget: domTarget,
+    // The config object passed to useGesture has drag, wheel, scroll, pinch and move keys
+    // for specific gesture options. See here for more details.
+    // drag: {
+    //   bounds,
+    //   rubberband: true,
+    // }
+  }};
 }
