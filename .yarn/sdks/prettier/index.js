2d7a9b49c7859569d5d3029a37f8001e3579c388
Joao Maio <10330349+JMaio@users.noreply.github.com>
2020-12-25 20:06:13 +0000

    fix(lint): update prettier settings

--- .yarn/sdks/prettier/index.js
+++ .yarn/sdks/prettier/index.js
@@ -1,8 +1,8 @@
 #!/usr/bin/env node
 
-const {existsSync} = require(`fs`);
-const {createRequire, createRequireFromPath} = require(`module`);
-const {resolve, dirname} = require(`path`);
+const { existsSync } = require(`fs`);
+const { createRequire, createRequireFromPath } = require(`module`);
+const { resolve } = require(`path`);
 
 const relPnpApiPath = "../../../.pnp.js";
 
@@ -15,14 +15,20 @@ if (existsSync(absPnpApiPath)) {
     require(absPnpApiPath).setup();
   }
 
-  const pnpifyResolution = require.resolve(`@yarnpkg/pnpify`, {paths: [dirname(absPnpApiPath)]});
-  if (typeof global[`__yarnpkg_sdk_is_using_pnpify__`] === `undefined`) {
-    Object.defineProperty(global, `__yarnpkg_sdk_is_using_pnpify__`, {configurable: true, value: true});
+  let pnpifyResolution;
+  try {
+    pnpifyResolution = absRequire.resolve(`@yarnpkg/pnpify`);
+  } catch (err) { }
 
-    process.env.NODE_OPTIONS += ` -r ${pnpifyResolution}`;
+  if (pnpifyResolution) {
+    if (typeof global[`__yarnpkg_sdk_is_using_pnpify__`] === `undefined`) {
+      Object.defineProperty(global, `__yarnpkg_sdk_is_using_pnpify__`, { configurable: true, value: true });
 
-    // Apply PnPify to the current process
-    absRequire(pnpifyResolution).patchFs();
+      process.env.NODE_OPTIONS += ` -r ${pnpifyResolution}`;
+
+      // Apply PnPify to the current process
+      absRequire(pnpifyResolution).patchFs();
+    }
   }
 }
 
